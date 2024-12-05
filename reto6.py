0c59bd9bfdc9b0399eb88721ee80a6f7fdac7882
alex7est <alexanderestrella82@gmail.com>
2024-12-04 20:17:33 -0500

    Reto 7

--- reto6.py
+++ reto6.py
@@ -27,7 +27,37 @@ class auto:
                 print("Ya estoy usado")
             else:
                 print("¡Ya déjame descansar por favor!")
-
+    
+    #---Reto 7---#                
+    @classmethod
+    def auto_toyota(cls):
+        marca = "Toyota"
+        modelo = "Corolla"
+        año = 2020
+        return auto(marca, modelo, año)
+    
+    @staticmethod
+    def mismo_kilometraje(auto1: 'auto', auto2: 'auto'):
+        if auto1.kilometraje == auto2.kilometraje:
+            print("Tienen el mismo kilometraje")
+        else:
+            print("No tienen el mismo kilometraje")
+    
+    @classmethod
+    def auto_chevrolet(cls):
+        marca = "Chevrolet"
+        modelo = "Corvette"
+        año = 2024
+        return auto(marca, modelo, año)
+    
+    @staticmethod
+    def es_auto_nuevo(auto: 'auto'):
+        if auto.año >= 2024:
+            print("El auto es nuevo")
+        else:
+            print("El auto no es nuevo")
+            
+"""
 mi_auto = auto("Toyota", "Corolla", 2020, 0)
 
 mi_auto.mostrar_informacion()
@@ -40,3 +70,13 @@ print(f"Kilometraje actual: {mi_auto.kilometraje}")
 mi_auto.estado_auto()
 mi_auto.actualizar_kilometraje(20000)
 mi_auto.realizar_viaje(-500)
+"""
+
+#---Reto 7---#
+auto_toyota = auto.auto_toyota()
+auto_chevrolet = auto.auto_chevrolet()
+
+auto.es_auto_nuevo(auto_toyota)
+auto.es_auto_nuevo(auto_chevrolet)
+
+auto.mismo_kilometraje(auto_toyota, auto_chevrolet)
