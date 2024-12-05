3fe6bd02c9716528dd57cd3291a39c3238f050c9
alex7est <alexanderestrella82@gmail.com>
2024-12-05 14:16:53 -0500

    Reto 8

---
+++ Laptop_Business.py
@@ -0,0 +1,40 @@
+from Laptop import Laptop
+import random
+
+class Laptop_Business(Laptop):
+    def __init__(self, marca, procesador, memoria, almacenamiento, duracion_bateria, costo=500, impuesto=10):
+        super().__init__(marca, procesador, memoria, costo, impuesto)
+        self.almacenamiento = almacenamiento
+        self.duracion_bateria = duracion_bateria
+
+    def realizar_diagnostico_sistema(self):
+        diagnostico_base = super().realizar_diagnostico_sistema()
+        diagnostico_base["ALMACENAMIENTO"] = "OK" if self.almacenamiento >= 256 else "Aumentar almacenamiento"
+        diagnostico_base["DURACIÓN DE BATERÍA"] = "OK" if self.duracion_bateria >= 4 else "Verificar batería"
+        
+        return diagnostico_base
+
+    def verificar_conectividad_red(self):
+        conectividad = {
+            "Wi-Fi Disponible": random.choice([True, False]),
+            "Acceso a Servidores Empresariales": random.choice([True, False]),
+            "Latencia de Red Estable": random.choice([True, False])
+        }
+        return conectividad
+
+
+laptop_empresarial = Laptop_Business(
+    marca="Lenovo",
+    procesador="i7",
+    memoria=16,
+    almacenamiento=512,
+    duracion_bateria=8,
+    costo=1000,
+    impuesto=50
+)
+
+diagnostico = laptop_empresarial.realizar_diagnostico_sistema()
+print(f"Diagnóstico del sistema empresarial: {diagnostico}")
+
+conectividad = laptop_empresarial.verificar_conectividad_red()
+print(f"Resultados de conectividad de red: {conectividad}")
